f640e4955b43a78a1744a96167463f2a8c1f3c5a
Ch3k0 <sergioarieljuarez@gmail.com>
2024-05-14 22:18:05 +0200

    add material ui for scss Angular framework blog project menu button its ok right now!

--- src/app/components/responsive-app-bar/responsive-app-bar.component.ts
+++ src/app/components/responsive-app-bar/responsive-app-bar.component.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {MatToolbar, MatToolbarRow} from "@angular/material/toolbar";
 import {MatIcon} from "@angular/material/icon";
+import {MatMenu, MatMenuItem, MatMenuTrigger} from "@angular/material/menu";
+import {RouterLink} from "@angular/router";
+import {MatButton} from "@angular/material/button";
 
 @Component({
   selector: 'app-responsive-app-bar',
@@ -9,10 +12,13 @@ import {MatIcon} from "@angular/material/icon";
     MatToolbar,
     MatToolbarRow,
     MatIcon,
+    MatMenu,
+    MatMenuTrigger,
+    RouterLink,
+    MatButton,
+    MatMenuItem,
   ],
   templateUrl: './responsive-app-bar.component.html',
   styleUrl: './responsive-app-bar.component.scss'
 })
-export class ResponsiveAppBarComponent {
-
-}
+export class ResponsiveAppBarComponent {}
